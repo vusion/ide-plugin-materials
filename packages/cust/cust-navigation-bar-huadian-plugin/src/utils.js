82fb251a891b5dd2245f0575eb84e21b1ee1325c
zhuanghengheng <zhuanghengheng@corp.netease.com>
2025-07-29 16:31:55 +0800

    refactor(cust-navigation-bar-huadian-plugin): 注释掉不需要的导航栏节点
    移除插件管理、共享管理、逻辑和数据节点，保留流程节点

--- packages/cust/cust-navigation-bar-huadian-plugin/src/utils.js
+++ packages/cust/cust-navigation-bar-huadian-plugin/src/utils.js
@@ -6,19 +6,19 @@ const hideNodes = [
   "top/right/connector-center", // 集成
   "more-menu/config-manager", // 应用配置
   "more-menu/i18n-config", // 国际化多语言
-  "more-menu/plugin-manager", // 插件管理
+  // "more-menu/plugin-manager", // 插件管理
   "top/right/release", // 发布
 
   // 更多
   // 自定义主题样式
   "theme-setting/aside",
   // "theme-setting/header/search", // 组件搜索 （⚠️：使用hide方法不生效，改为更高优先级的skeleton-portal组件的hide）
-  "more-menu/share-manager", // 共享管理
+  // "more-menu/share-manager", // 共享管理
 
   // 侧边栏
-  "left-side-nav/main/Logic", // 逻辑
-  "left-side-nav/main/Data", // 数据
-  "left-side-nav/main/Process", // 逻辑
+  // "left-side-nav/main/Logic", // 逻辑
+  // "left-side-nav/main/Data", // 数据
+  "left-side-nav/main/Process", // 流程
   // "left-side-nav/rest/Feedback", // 客服
   // "left-side-nav/rest/Help", // 帮助
   // "left-side-nav/rest/Complaint", // 吐槽
